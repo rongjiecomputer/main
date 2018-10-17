7156a7889584750ec29e2c9b6adf8cca8442ef3d
Loo Rong Jie <loorongjie@gmail.com>
2018-10-17 13:21:58 +0800

    Fix style

--- src/main/java/seedu/planner/storage/JsonModuleInfoStorage.java
+++ src/main/java/seedu/planner/storage/JsonModuleInfoStorage.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.google.common.collect.ImmutableMap;
-
 import seedu.planner.commons.exceptions.DataConversionException;
 import seedu.planner.commons.util.JsonUtil;
 import seedu.planner.model.module.ModuleInfo;
